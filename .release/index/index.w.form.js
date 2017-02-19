104374760c41769a4124fc5f89bcc736b8c28a68
jancnzhangmin <jancnzhangmin@hotmail.com>
2017-02-19 16:53:16 +0800

    init2

---
+++ .release/index/index.w.form.js
@@ -0,0 +1,25 @@
+define(function(require){
+require('$model/UI2/system/components/justep/model/model');
+require('$model/UI2/system/components/justep/contents/content');
+require('$model/UI2/system/components/justep/loadingBar/loadingBar');
+require('$model/UI2/system/components/justep/button/button');
+require('$model/UI2/system/components/justep/scrollView/scrollView');
+require('$model/UI2/system/components/justep/list/list');
+require('$model/UI2/system/components/justep/titleBar/titleBar');
+require('$model/UI2/system/components/justep/panel/child');
+require('$model/UI2/system/components/justep/contents/contents');
+require('$model/UI2/system/components/justep/window/window');
+require('$model/UI2/system/components/justep/button/buttonGroup');
+require('$model/UI2/system/components/justep/panel/panel');
+var __parent1=require('$model/UI2/system/lib/base/modelBase'); 
+var __parent0=require('$model/UI2/wavehill/index'); 
+var __result = __parent1._extend(__parent0).extend({
+	constructor:function(contextUrl){
+	this.__sysParam='true';
+	this.__contextUrl=contextUrl;
+	this.__id='';
+	this.__cid='cQvqAbm';
+	this._flag_='c636c746cf4f26080438087f299238cf';
+	this.callParent(contextUrl);
+}}); 
+return __result;});
--- index.js
+++ index.js
@@ -2,9 +2,47 @@ define(function(require){
 	var $ = require("jquery");
 	var justep = require("$UI/system/lib/justep");
 
+	require("cordova!cordova-plugin-device");
+	require("cordova!com.justep.cordova.plugin.weixin.v3");
+	require("cordova!com.justep.cordova.plugin.alipay");
+
 	var Model = function(){
 		this.callParent();
 	};
+
+	Model.prototype.modelLoad = function(event){
+    var tokenUrl="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx5726c31c9832f709&secret=1452d18439c24dc52b19df787d035c88";
+
+
+
+    $.ajax({
+        url:tokenUrl,
+        type:"get",
+        dataType:"JSONP",
+        success:function(data){
+            var token=data.access_token;
+            var openid=data.openid;
+            Get_User_Info(token,openid);
+        }
+    });
+
+
+
+
+	};
+	
+	function Get_User_Info(token,openid)
+	{
+	var access_url="https://api.weixin.qq.com/cgi-bin/user/info?access_token="+token+"&openid="+openid+"&lang=zh_CN"; 
+	    $.ajax({
+        url:access_url,
+        type:"get",
+        dataType:"JSONP",
+        success:function(data){
+alert(data);
+        }
+    });
+	}
 
 	return Model;
 });
